c23e8f754da7fbf27069418d62d7e92382b6f7e9
sexposito <sergi123455@gmail.com>
2021-03-30 14:39:06 -0600

    Solucion fianl de los valores truthy

--- truthy.js
+++ truthy.js
@@ -1,11 +1,20 @@
 function parameters(a,b,c,d) {
     const obj = {}
-    for (i = 0; i < arguments.length; i++) {
-        if(arguments[i]) {
-            obj[arguments[i]] = arguments[i]
-        }
-    }
+    a ? obj.a = a : ''
+    b ? obj.b = b : ''
+    c ? obj.c = c : ''
+    d ? obj.d = d : ''
+
+    // No me gusto mi solucion por que podria hacer un ciclo pero no supe
+    // como agarrar el nombre de la variable para meterlo en el objeto
+
+
+    // for (i = 0; i < arguments.length; i++) {
+    //     if(arguments[i]) {
+    //         obj[arguments[i]] = arguments[i]
+    //     }
+    // }
     console.log(obj)
 }
 
-parameters(1,'jhjio',null,4,{})
+parameters(1,'jhjio',0,4)
